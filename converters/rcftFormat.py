d308f7f7be60074124a02daf122c99addaf4f249
Frédéric Lang <frederic.ln.lang@irisa.fr>
2025-05-12 11:12:04 +0200

    input as file

--- converters/rcftFormat.py
+++ converters/rcftFormat.py
@@ -1,4 +1,6 @@
-def parse_context(lines):
+import sys
+
+def parse_incidence(lines):
     headers = lines[0].split('|')[1:]
     headers = [header.strip() for header in headers]
 
@@ -11,42 +13,32 @@ def parse_context(lines):
     return relations
 
 
-data = """
-FormalContext person
-algo fca
-|  |male|female|
-|Jean|x     |  |
-|Julie|      |x     |
-|Julien|x     |      |
-|Jeanne|      |x     |
-|Bob|x     |      |
-|Anne|      |x     |
-
-RelationalContext love
-source person
-target person
-scaling exist 
-|  |Jean|Julie|Julien|Jeanne|Bob|Anne|
-|Jean|  |x |  |  |  |  |
-|Julie|  |  |x |  |  |  |
-|Julien|  |  |  |x |  |  |
-|Jeanne|x |  |  |  |  |  |
-|Bob|  |  |  |  |  |x |
-|Anne|  |  |  |  |x |  |
-"""
-
 def read_data(data):
     entries = data.strip().split('\n\n')
     for entry in entries:
         lines = entry.strip().split('\n')
         fst = lines[0].strip().split(' ')
         if fst[0] == "FormalContext":
-            relations=parse_context(lines[2:])
+            relations=parse_incidence(lines[2:])
             print(relations)
         elif fst[0] == "RelationalContext":
-            relations=parse_context(lines[4:])
+            relations=parse_incidence(lines[4:])
             print(relations)
         else:
             raise "unknown context"
 
-read_data(data)
+
+def main(filepath):
+    with open(filepath, 'r') as file:
+        data = file.read()
+        read_data(data)
+        
+
+if __name__ == "__main__":
+    # Vérifiez que des arguments ont été passés
+    if len(sys.argv) > 1:
+        # Le premier argument est à l'index 1 (sys.argv[0] est le nom du script)
+        filepath = sys.argv[1]
+        main(filepath)
+    else:
+        print("no file passed as input")
