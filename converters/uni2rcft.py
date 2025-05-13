be6952e3f73c208256517d623a26b143ffcdbfdb
Frédéric Lang <frederic.ln.lang@irisa.fr>
2025-05-13 17:29:49 +0200

    uni2rcft

--- converters/uni2rcft.py
+++ converters/uni2rcft.py
@@ -2,8 +2,51 @@ import sys
 import unicontext as unicontext
 
 
+def printFormalContext(name, ctx):
+    print("FormalContext", name)
+    print("algo fca")
+    header= "||"
+    for a in ctx.attributes:
+        header+=a
+        header+="|"
+    print(header)
+    for obj, rel in ctx.incidence.items():
+        line = "|" + obj + "|"
+        for a in ctx.attributes:
+            if a in rel:
+                line+= "x|"
+            else:
+                line+= "|"
+        print(line)
+
+def printRelContext(name, ctx, source, target):
+    print("RelationalContext", name)
+    print("source", ctx.domain)
+    print("target", ctx.range)
+    print("scaling exist")
+    header= "||"
+    for o in target:
+        header+=o
+        header+="|"
+    print(header)
+    for obj in source:
+        rel = ctx.incidence[obj]
+        line = "|" + obj + "|"
+        for t in target:
+            if t in rel:
+                line+= "x|"
+            else:
+                line+= "|"
+        print(line)
+
+
 def printCtx(model):
-    print("ok")
+    for name, ctx in model.formalContexts.root.items():
+        printFormalContext(name, ctx)
+        print("")
+    for name, ctx in model.relationalContexts.root.items():
+        printRelContext(name, ctx, model.categories.root[ctx.domain], model.categories.root[ctx.range])
+        print("")
 
 def validateModel(model):
     for name, ctx in model.relationalContexts.root.items():
