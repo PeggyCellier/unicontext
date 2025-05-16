6e0eafc43200b9d83a29c6b0b01daf417942e7f0
Frédéric Lang <frederic.ln.lang@irisa.fr>
2025-05-16 18:03:44 +0200

    uni2p

--- converters/uni2p.py
+++ converters/uni2p.py
@@ -1,16 +1,65 @@
 import sys
 import unicontext as unicontext
 
+def stringify(l):
+    if isinstance(l, str):
+        return l
+    elif isinstance(l, list):
+        s=""
+        if len(l) == 0:
+            return s
+        elif len(l) == 1:
+            return l[0]
+        else:
+            s += l[0]
+            for item in l[1:]:
+                s+= (" " + item)
+            return s
+    else:
+        print("unrecognized incidence")
 
+def computePformat(model):
+    categories = {}
+    for name, c in model.categories.root.items():
+        categories[name] = {element: [] for element in c}
+    for name, fc in model.formalContexts.root.items():
+        for obj, attr in fc.incidence.items():
+            categories[fc.domain][obj] += attr
+    for name, rc in model.relationalContexts.root.items():
+        def prefixAndStringify(l):
+            return name + " " + stringify(l)
+        for obj, rel in rc.incidence.items():
+            categories[rc.domain][obj] += list(map(prefixAndStringify, rel))
+    print(categories)
 
-def printCtx(data):
-    unicontext.printUniContext(data)
+def printCtx(model):
+    print("%", model.name)
+    print(":-")
+    for name, fc in model.formalContexts.root.items():
+        for obj in model.categories.root[fc.domain]:
+            rel = fc.incidence[obj]
+            if len(rel) == 0:
+                relString = " [ ]"
+                print(obj, ":", relString, ",")
+            elif len(rel) == 1:
+                relString= "[ " + rel[0] + " ]"
+                print(obj, ":", relString, ",")
+            else:
+                relString= "[ " + rel[0]
+                for attr in rel[1:]:
+                    relString += (", " + attr)
+                relString += " ]"
+                print(obj, ":", relString, ",")
+    for name, rc in model.relationalContexts.root.items():
+        for obj in model.categories.root[rc.domain]:
+            print(obj, ":")
+    print(".")
 
 
 def main(filepath):
     with open(filepath, 'r') as file:
         data = unicontext.DataModel.model_validate_json(file.read())
-        printCtx(data)
+        computePformat(data)
 
 
 if __name__ == "__main__":
