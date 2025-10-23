4069bbeb1aed1b27b4d014269e19447180a54a78
Frédéric Lang <frederic.ln.lang@irisa.fr>
2025-10-23 15:15:45 +0200

    uni2x calls now uni2cxt

--- converters/uni2cxt.py
+++ converters/uni2cxt.py
@@ -37,6 +37,7 @@ def printCxt(model):
         printInOutput(a)
     printMatrix(objects, fc.attributes, fc.incidence)
 
+# this function aims to ensure that the model is convertible to a .cxt file
 def validateModel(model):
     if len(model.categories.root) == 1:
         if len(model.formalContexts.root) == 1:
@@ -52,11 +53,43 @@ def validateModel(model):
         print("uni2cxt : only one category allowed")
         return False
 
+# this function aims to build a model acceptable by uni2cxt from any model
+def lintModel(model):
+    #first remove all relational contexts
+    model.relationalContexts.root.clear()
+    
+    # remove all the categories but the first one
+    dictionary = model.categories.root
+    first_key = next(iter(dictionary))
+    keys_to_remove = [key for key in dictionary if key != first_key]
+    for key in keys_to_remove:
+        del dictionary[key]
+
+    # remove all formal context except a formal context which domain is the first category
+    dictionary2 = model.formalContexts.root
+    first_key_reach = False
+    keys_to_remove = [key for key in dictionary2]
+    cat = dictionary[first_key]
+    for key in keys_to_remove:
+        if dictionary2[key].domain == first_key and not first_key_reach:
+            first_key_reach = True
+        else :
+            del dictionary2[key]
+
+    # if all formal contexts have been removed, build a blank formal context
+    if len(dictionary2) == 0:
+        dictionary2["objects"] = unicontext.FormalContext(
+            domain="objects", 
+            attributes=["object"],
+            incidence={key:["object"] for key in cat})
+    
+    # unicontext.printUniContext(model)
 
 
 def main(filepath):
     with open(filepath, 'r') as file:
         data = unicontext.DataModel.model_validate_json(file.read())
+        lintModel(data)
         if validateModel(data):
             printCxt(data)
             basefilepath, file_extension = os.path.splitext(filepath)
--- converters/uni2x.py
+++ converters/uni2x.py
@@ -5,12 +5,15 @@ import uni2cxt
 import uni2rcft
 
 if __name__ == "__main__":
-    # Vérifiez que des arguments ont été passés
+    # verify that command has one argument
     if len(sys.argv) > 1:
-        # Le premier argument est à l'index 1 (sys.argv[0] est le nom du script)
+        #  (sys.argv[0] is the name of the script) filepath is sys.argv[1]
         filepath = sys.argv[1]
         uni2p.main(filepath)
         uni2rcft.main(filepath)
+        # warning : only unicontext object without relational data are accepted by uni2cxt
+        # therefore uni2cxt will rebuild a context without relational data and with only one category
+        # and one formal context from your context
         uni2cxt.main(filepath)
     else:
         print("no file passed as input")
